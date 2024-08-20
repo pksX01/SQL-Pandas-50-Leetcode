aee3643b8238bdfb67147fccff0c5db0d37c2656
Pavan Kumar Singh <pksX01@gmail.com>
2024-08-20 13:44:24 +0530

    MySQL and Pandas solution for problem - customer visited but did not do transaction

---
+++ cust_visit_not_txn_v3.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    merged_df = visits.merge(transactions, how = "left", on = "visit_id")
+    filtered_df = merged_df.query('transaction_id.isna()')
+    return filtered_df.groupby("customer_id").size().reset_index(name='count_no_trans')
